c7855709342b2fd6505fcec4a54c8063a37de438
Carrison57 <calebarrison@gmail.com>
2016-09-08 11:36:51 -0400

    isbn app created

---
+++ isbn_app.rb
@@ -0,0 +1,18 @@
+require "sinatra"
+require_relative "new_isbn.rb"
+
+get "/" do
+	erb :get_isbn
+end
+
+post "/isbn" do
+  isbn = params[:isbn_number]
+  result = valid_isbn_10_or_13(isbn)
+  	if result == true
+  		result = "Valid"
+  	else result == false
+  		result = "Invalid"
+  	end
+
+	"Your isbn number #{result}"
+end
